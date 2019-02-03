edae4b8501fa10c7781a4da836ca9bee78d570c4
kaushal.agarwal2016@vitstudent.ac.in <kaushalagarwal79@gmail.com>
2019-02-03 23:03:20 +0530

    Portal A
    Portal A layout and player controller added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float rotate_speed;
+    public float translate_speed;
+
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Rotate(0, x*rotate_speed, 0);
+        transform.Translate(0, 0, z*translate_speed);
+
+    }
+}
